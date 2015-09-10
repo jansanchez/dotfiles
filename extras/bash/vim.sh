1987c8f19abe64148636f68e4c911f45e673753b
Jan Sanchez <joejansanchez@gmail.com>
2015-09-09 23:12:42 -0500

    feat(vim): agrego plugin vim-coffee-script

--- extras/bash/vim.sh
+++ extras/bash/vim.sh
@@ -25,20 +25,20 @@ if [ ! -d "vim-commentary" ]; then
     git clone git://github.com/tpope/vim-commentary.git vim-commentary
 fi
 
-
 # Install vim-numbers
 if [ ! -d "vim-numbers" ]; then
     git clone https://github.com/myusuf3/numbers.vim.git vim-numbers
 fi
-
 
 # Install vim-easymotion
 if [ ! -d "vim-easymotion" ]; then
     git clone git://github.com/Lokaltog/vim-easymotion.git vim-easymotion
 fi
 
-
-
+# Install vim-coffee-script
+if [ ! -d "vim-coffee-script" ]; then
+    git clone https://github.com/kchmck/vim-coffee-script.git vim-coffee-script
+fi
 
 
 
