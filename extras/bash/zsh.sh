c50ad04a8886e7266f8dd244410bf58c438375d6
Jan Sanchez <joejansanchez@gmail.com>
2015-09-16 15:40:39 -0500

    feat(zsh): agrego plugins

--- extras/bash/zsh.sh
+++ extras/bash/zsh.sh
@@ -10,9 +10,22 @@ sudo chsh -s /bin/zsh
 
 cp ../.oh-my-zsh/themes/*.zsh-theme ~/.oh-my-zsh/themes/
 
+# Zsh Plugins
+
 cd ~/.oh-my-zsh/custom/plugins
 
+# zsh-syntax-highlighting
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+
+# history-substring-search
+git clone https://github.com/zsh-users/zsh-history-substring-search.git
+
+# zsh-vim-mode
+git clone https://github.com/sharat87/zsh-vim-mode.git
+
+# zsh-url-highlighter
+git clone https://github.com/ascii-soup/zsh-url-highlighter.git 
+
 
 echo "[Zsh instalado]"
 
